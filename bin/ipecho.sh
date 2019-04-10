1741d6af70000fc142708e3cfd6d61c2d48121c0
Ivo Woltring <Ivo@IvoNet.nl>
2019-04-10 23:50:04 +0200

    reformat

--- bin/ipecho.sh
+++ bin/ipecho.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-#docker exec $(docker ps |grep vpn|awk '{print $1}') curl -s https://ipecho.net/plain
+# docker exec $(docker ps |grep vpn|awk '{print $1}') curl -s https://ipecho.net/plain
 
 docker exec nordvpn curl -s https://ipecho.net/plain
